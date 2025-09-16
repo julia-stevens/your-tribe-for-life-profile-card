ae0e62eb13f7fa0a2eec021a0e73b5c38b06ffc1
Julia Stevens <juliastevens02@gmail.com>
2025-09-16 16:24:38 +0200

    #15 #16 component + projects dynamischer

---
+++ src/lib/components/ProjectLink.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let href = "#";
+    export let name = "";
+    import arrowLinkLight from "$lib/assets/arrow-link-lightmode.svg"; 
+</script>
+
+<a href={href}>
+    <article class="row">
+        <h3>{name}</h3>
+        <img 
+            src={arrowLinkLight} 
+            alt="Arrow link"
+            width="20"
+            height="20"
+            style="object-fit: cover;"
+        >
+    </article>
+</a>
+
+<style>
+    article {
+        justify-content: space-between;
+        border: 2px solid var(--border-primary);
+        width: 70vw;
+        max-width: 20rem;
+        padding: .5em 1em;
+    }
+
+    article img{
+        width: 1rem;
+        height: 1rem;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        article img {
+            content: url("$lib/assets/arrow-link-darkmode.svg");
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,104 +1,97 @@
 <script>
-    import arrowLinkLight from "$lib/assets/arrow-link-lightmode.svg";
+    // COMPONENTS
+    import ProjectLink from "$lib/components/ProjectLink.svelte";
+
+    // ASSETS
 	import avatarLight from "$lib/assets/avatar-lightmode.svg";
 	import avatarHeadLight from "$lib/assets/avatar-head-lightmode.svg";
 	import iconLight from "$lib/assets/icon-lightmode.svg";
     
+    // DATA
     let { data } = $props(); 
     const member = data.member; 
-</script>
-    <section class="introduction">
-        <h1 class="column">
-            <span>Hello.</span>
-            <span>I'm Julia.</span>
-        </h1>
-        <img 
-            src={avatarLight} 
-            alt="Avatar"
-            width="100"
-            height="100"
-            style="object-fit: cover;"
-        >
-    </section>
 
-    <section class="about column">
+    // PROJECTS
+    const projects = [
+        {
+            name: member.custom.projectName1Sem3,
+            href: member.custom.projectLink1Sem3
+        },
+        {
+            name: member.custom.projectName2Sem3,
+            href: member.custom.projectLink2Sem3
+        }
+    ]
+</script>
+
+<section class="introduction">
+    <h1 class="column">
+        <span>Hello.</span>
+        <span>I'm Julia.</span>
+    </h1>
+    <img 
+        src={avatarLight} 
+        alt="Avatar"
+        width="100"
+        height="100"
+        style="object-fit: cover;"
+    >
+</section>
+
+<section class="about column">
+    <h2>
+        About
+    </h2>
+    <p>
+        {member.custom.bioSem3}
+    </p>
+</section>
+
+<section class="projects column column">
+    <div class="projects_description column">
         <h2>
-            About
+            Projects
         </h2>
         <p>
-            {member.custom.bioSem3}
+            {member.custom.projectsSem3}
         </p>
-    </section>
+    </div>
+    <div class="projects_content column">
+        {#each projects as project}
+        <ProjectLink
+            href={project.href}
+            name={project.name}
+        />
+        {/each}
+    </div>
+</section>
 
-    <section class="projects column column">
-        <div class="projects_description column">
-            <h2>
-                Projects
-            </h2>
-            <p>
-                {member.custom.projectsSem3}
-            </p>
-        </div>
-        <div class="projects_content column">
-            <a 
-                href="{member.custom.projectLink1Sem3}"
-                
-            >
-                <article class="row">
-                    <h3>{member.custom.projectName1Sem3}</h3>
-                    <img 
-                        src={arrowLinkLight} 
-                        alt=""
-                        width="20"
-                        height="20"
-                        style="object-fit: cover;"
-                    >
-                </article>
-            </a>
-            <a 
-                href="{member.custom.projectLink2Sem3}"
-                
-            >
-                <article class="row">
-                    <h3>{member.custom.projectName2Sem3}</h3>
-                    <img 
-                        src={arrowLinkLight} 
-                        alt=""
-                        width="20"
-                        height="20"
-                        style="object-fit: cover;"
-                    >                    
-                </article>
-            </a>
-        </div>
-    </section>
+<section class="funfacts column">
+    <h2>Fun facts</h2>
 
-    <section class="funfacts column">
-        <h2>Fun facts</h2>
-
-        <div class="funfacts-inner">
-            <dl>
-                <dt>Favorite color</dt>
-                <dd>{member.fav_color}</dd>
-            </dl>
-            <dl>
-                <dt>Favorite Emoji</dt>
-                <dd>{member.fav_emoji}</dd>
-            </dl>
-            <dl>
-                <dt>Favorite tag</dt>
-                <dd>{member.fav_tag}</dd>
-            </dl>
-            <dl>
-                <dt>Favorite feature</dt>
-                <dd>{member.fav_feature}</dd>
-            </dl>
-            <dl>
-                <dt>Favorite attribute</dt>
-                <dd>{member.fav_attribute}</dd>
-            </dl>
-        </div>
-    </section>
+    <div class="funfacts-inner">
+        <dl>
+            <dt>Favorite color</dt>
+            <dd>{member.fav_color}</dd>
+        </dl>
+        <dl>
+            <dt>Favorite Emoji</dt>
+            <dd>{member.fav_emoji}</dd>
+        </dl>
+        <dl>
+            <dt>Favorite tag</dt>
+            <dd>{member.fav_tag}</dd>
+        </dl>
+        <dl>
+            <dt>Favorite feature</dt>
+            <dd>{member.fav_feature}</dd>
+        </dl>
+        <dl>
+            <dt>Favorite attribute</dt>
+            <dd>{member.fav_attribute}</dd>
+        </dl>
+    </div>
+</section>
 
 
 <style>
@@ -124,25 +117,6 @@
 
     .projects, .projects_content {
         gap: var(--gap-s);
-    }
-
-    .projects article {
-        justify-content: space-between;
-        border: 2px solid var(--border-primary);
-        width: 70vw;
-        max-width: 20rem;
-        padding: .5em 1em;
-    }
-
-    .projects article img{
-        width: 1rem;
-        height: 1rem;
-    }
-
-    @media (prefers-color-scheme: dark) {
-        .projects article img {
-            content: url("$lib/assets/arrow-link-darkmode.svg");
-        }
     }
 
     /* FUN FACTS */
